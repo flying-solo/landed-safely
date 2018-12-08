ff6c73d374eb1b1669a454c4d9152b9942b75766
Faiq Allam <faiq.kaboel@gmail.com>
2018-12-08 13:40:19 +0700

    Init Project

---
+++ src/main/java/lds/login/User.java
@@ -0,0 +1,13 @@
+/*
+ *  * @author kaboel
+ *  * Muhammad Faiq Allam | faiq.kaboel@gmail.com
+ */
+
+package lds.login;
+
+
+public class User {
+    private String id;
+    private String username;
+    private char[] password;
+}
