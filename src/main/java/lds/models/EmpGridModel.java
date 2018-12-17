d6b0a6db4316b655075d02fb63b5a99c85dacbbc
Faiq Allam <faiq.kaboel@gmail.com>
2018-12-17 23:27:16 +0700

    Edit EmpGridModel change Table Model structure

--- src/main/java/lds/models/EmpGridModel.java
+++ src/main/java/lds/models/EmpGridModel.java
@@ -6,29 +6,23 @@
 package lds.models;
 
 import java.util.ArrayList;
-import javax.swing.table.AbstractTableModel;
 import lds.entities.Employee;
+import javax.swing.table.AbstractTableModel;
 
 
 public class EmpGridModel extends AbstractTableModel {
     private final String[] columns;
     private ArrayList<Employee> data;
-    private int rows;
     
     public EmpGridModel(ArrayList<Employee> data) {
         this.columns = new String[] {
             "Name", "Gender", "Email", "Address", "Phone", "Position", "Status"
         };
         this.data = data;
-        this.rows = -1;
     }
     
     public void setData(ArrayList<Employee> data) {
         this.data = data;
-    }
-    
-    public void setRowCount(int count) {
-        this.rows = count;
     }
     
     @Override
@@ -38,25 +32,43 @@ public class EmpGridModel extends AbstractTableModel {
 
     @Override
     public int getRowCount() {
-        if(this.rows == -1) {
-            this.rows = this.data.size();
-        } else if(this.data.isEmpty()) {
-            this.rows = 0;
-        }
-        return this.rows;
+        return this.data.size();
     }
 
     @Override
     public Object getValueAt(int row, int col) {
+        String gender;
+        if(this.data.get(row).getGender() == 1) {
+            gender = "Laki -laki";
+        } else {
+            gender = "Perempuan";
+        }
+        
+        String position;
+        switch(this.data.get(row).getPosition()) {
+            case 1  : position = "Courier"; break;
+            case 2  : position = "Staff"; break;
+            case 3  : position = "Supervisor"; break;
+            default : position = "Not Positioned Yet";
+        }
+        
+        String status;
+        if(this.data.get(row).getActivation() == 1) {
+            status = "Active";
+        } else {
+            status = "Non-Active";
+        }
+        
+        
         Object tmp = null;
         switch(col) {
             case 0 : tmp = this.data.get(row).toString(); break;
-            case 1 : tmp = this.data.get(row).getGender(); break;
+            case 1 : tmp = gender; break;
             case 2 : tmp = this.data.get(row).getEmail(); break;
             case 3 : tmp = this.data.get(row).getAddress(); break;
             case 4 : tmp = this.data.get(row).getPhone(); break;
-            case 5 : tmp = this.data.get(row).getPosition(); break;
-            case 6 : tmp = this.data.get(row).getActivation(); break;
+            case 5 : tmp = position; break;
+            case 6 : tmp = status; break;
             case 7 : tmp = this.data.get(row).getId(); break;
         }
         return tmp;
