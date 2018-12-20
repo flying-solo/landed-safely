d54f917d328d9746bf376562122388968794effd
Faiq Allam <faiq.kaboel@gmail.com>
2018-12-20 18:32:59 +0700

    Add Province for Province Object Handler

---
+++ src/main/java/lds/lib/Entities/Province.java
@@ -0,0 +1,32 @@
+/*
+ *  * @author kaboel
+ *  * Muhammad Faiq Allam | faiq.kaboel@gmail.com
+ */
+
+package lds.lib.Entities;
+
+
+public class Province {
+    private final int id;
+    private final String province;
+    
+    public Province(int id, String province) {
+        this.id = id;
+        this.province = province;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    @Override
+    public String toString() {
+        return this.province;
+    }
+    
+    
+}
