9f75041dc9d977107d55013d0dab8b7e5f6bea6e
Faiq Allam <faiq.kaboel@gmail.com>
2018-12-21 07:28:10 +0700

    Edit CityRegion change name to CityRegency and change DAO structure

---
+++ src/main/java/lds/lib/DAO/CityRegencyDAO.java
@@ -0,0 +1,18 @@
+/*
+ *  * @author kaboel
+ *  * Muhammad Faiq Allam | faiq.kaboel@gmail.com
+ */
+package lds.lib.DAO;
+
+import java.util.ArrayList;
+import lds.lib.Entities.CityRegency;
+import lds.lib.Entities.Province;
+
+/**
+ *
+ * @author kaboel
+ */
+public interface CityRegencyDAO {
+    ArrayList<CityRegency> getAllCir();
+    ArrayList<CityRegency> getCirByProv(Province prov);
+}
