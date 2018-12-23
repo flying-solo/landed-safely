63f5c4fc410bc363e3342643aeff21b33e5cc031
Faiq Allam <faiq.kaboel@gmail.com>
2018-12-23 23:36:46 +0700

    Edit Admin add constructor structure and getter & setter

--- src/main/java/lds/lib/Entities/Admin.java
+++ src/main/java/lds/lib/Entities/Admin.java
@@ -8,15 +8,15 @@ package lds.lib.Entities;
 
 public class Admin {
     private final int id_admin;
-    private final int id_employee;
+    private final String employee;
     private final String username;
     private final String password;
     private final String date_reg;
     private final int permit;
 
-    public Admin(int id_admin, int id_employee, String username, String password, String date_reg, int permit) {
+    public Admin(int id_admin, String employee, String username, String password, String date_reg, int permit) {
         this.id_admin = id_admin;
-        this.id_employee = id_employee;
+        this.employee = employee;
         this.username = username;
         this.password = password;
         this.date_reg = date_reg;
@@ -27,8 +27,8 @@ public class Admin {
         return id_admin;
     }
 
-    public Integer getId_employee() {
-        return id_employee;
+    public String getEmployee() {
+        return employee;
     }
 
     public String getUsername() {
