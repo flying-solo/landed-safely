ae3191bdd0dc902459371f7bc76e45b14292b450
kaboel <faiq.kaboel@gmail.com>
2018-12-25 01:21:55 +0700

    Edit Login , implement password encryption login

--- src/main/java/lds/lib/Libs/Login.java
+++ src/main/java/lds/lib/Libs/Login.java
@@ -6,67 +6,35 @@
 package lds.lib.Libs;
 
 import lds.lib.Entities.Admin;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import lds.lib.Controllers.AdminController;
 
 
 public class Login {
+    
+    private final AdminController admcon;
     private final String username;
     private final String password;
     
     public Login(String username, String password) {
+        this.admcon = new AdminController();
         this.username = username;
         this.password = password;
     }
     
-    private Admin setUser(ResultSet rs) {
-        Admin admin;
-        try {
-            admin = new Admin(
-                rs.getInt("id_admin"),
-                rs.getString("name"),
-                rs.getString("username"),
-                rs.getString("password"),
-                rs.getString("date_reg"),
-                rs.getInt("permit")
-            );
-        } catch (SQLException e) {
-            admin = null;
-        }
-        return admin;
-    }
-    
     public Admin isValid() {
-        Admin result = null;
         try {
-            Connection con = Conn.initConn();
-            PreparedStatement st = con.prepareStatement(
-                "SELECT " +
-                "    A.id_admin, " +
-                "    CONCAT(E.firstname, ' ', E.lastname) AS name, " +
-                "    A.username, " +
-                "    A.password, " +
-                "    A.date_reg, " +
-                "    A.permit " +
-                "FROM " +
-                "    t_admin AS A " +
-                "LEFT JOIN t_employee AS E " +
-                "ON " +
-                "    A.id_employee = E.id_employee " +
-                "WHERE " +
-                "    A.username = ? AND A.password = ?"
-            );
-            st.setString(1, username);
-            st.setString(2, password);
-            ResultSet rs = st.executeQuery();
-            if(rs.next()) {
-                result = setUser(rs);
+            Admin admin = this.admcon.getAdmByUsername(this.username);
+            if(admin != null) {
+                String pass = admin.getPassword();
+                String salt = admin.getSalt();
+                
+                if(Encryption.isVerified(this.password, pass, salt)) {
+                    return admin;
+                }
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             StringBuilder sb = new StringBuilder();
             sb.append("Error: ").append(e.getMessage()).append("\n");
             sb.append("Cannot login at the momment.");
@@ -74,6 +42,6 @@ public class Login {
             JFrame frame = new JFrame();
             JOptionPane.showMessageDialog(frame, sb, "LDS : Error", JOptionPane.ERROR_MESSAGE);
         }
-        return result;
+        return null;
     }
 }
