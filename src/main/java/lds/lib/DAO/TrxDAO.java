1e3587930fe945876e26a83b0f54e279cfbcf1b7
kaboel <faiq.kaboel@gmail.com>
2018-12-31 02:04:44 +0700

    Edit TrxDAO add getAllType and getAllServ interface method

--- src/main/java/lds/lib/DAO/TrxDAO.java
+++ src/main/java/lds/lib/DAO/TrxDAO.java
@@ -6,6 +6,8 @@ package lds.lib.DAO;
 
 import java.util.ArrayList;
 import lds.lib.Entities.Transact;
+import lds.lib.Entities.PackageType;
+import lds.lib.Entities.ServiceType;
 
 /**
  *
@@ -18,4 +20,8 @@ public interface TrxDAO {
     ArrayList<Transact> getTrxByDest(int id_dest);
     ArrayList<Transact> getTrxByServ(int id_serv);
     boolean insertTrx(Transact trx);
+    
+    ArrayList<PackageType> getAllType();
+    
+    ArrayList<ServiceType> getAllServ();
 }
